3380da743eca01c877361b61e97579916f83c573
votre_email_github <votre_email_github>
2024-04-05 08:55:49 +0200

    1

--- Javascript 2/exos.js
+++ Javascript 2/exos.js
@@ -1,18 +1,24 @@
 function saisie() {
-    let mot = prompt('Veuillez entrer un mot : ')
-    const motMin = mot.toLowerCase();
-    while (/[\d]/.test(motMin)) {
-        mot = prompt('Veuillez entrer un mot sans chiffres : ')
+    let mot = prompt('Veuillez entrer un mot : ');
+    while (!/^[a-zA-z]+$/.test(mot)) {
+        mot = prompt('Veuillez entrer un mot sans chiffres ni caractères speciaux : ');
     }
     palindrome(mot);
 }
-
-const palindrome = (motMin) => {
-    const tabSplitReverse = motMin.split('').reverse().join('');
-        console.log(motMin);
-        console.log(tabSplitReverse);
-    const compare = (motMin === tabSplitReverse) ? 'Gagné' : 'Perdu';
-        console.log(compare)
+const palindrome = (mot) => {
+    const motMin = mot.toLowerCase();
+    const motReverse = motMin.split('').reverse().join('');
+    const compare = (motMin === motReverse) ? 'est' : 'n\'est pas';
+    const reponse = `Le mot ${mot} ${compare} un palindrome`;
+    console.log(reponse);
+    newGame();
 }
-
+function newGame () {
+    let newGameStart = prompt('Voulez vous rejouer ? (O/N) : ');
+    while (!((/^[O]/||/^[N]/).test(newGameStart))) {
+        newGameStart = prompt('Veuillez entrer O ou N : ');
+    }
+    const restart = (newGameStart === "O") ? saisie() : 'Jeu terminé';
+    console.log(restart)
+}
 saisie();
--- Javascript 2/index.js
+++ Javascript 2/index.js
@@ -1,64 +1,9 @@
-// function ecrireUnMot () {
-//     // instructions
-//     console.log('un mot')
-// }
+console.log('Dom loaded');
 
-// // appel de la fonction
-// ecrireUnMot();
+let protocol = window.location.protocol;
+let host = window.location.hostname;
+let port = window.location.port;
 
+const myURL = `${protocol}//${host}:${port}`;
 
-// // fonction pair ou impair
-// function nombreEstPair(nombre) {
-//     const isPair = nombre % 2 === 0 ? 'pair' : 'impair';
-//     const reponse = `Le nombre ${nombre} est ${isPair}`;
-//     console.log(reponse);
-// } 
-
-// // fonction flechée (identique mais sous forme de constante)
-// const maFonction = (nombre) => {
-//     const isPair = nombre % 2 === 0 ? 'pair' : 'impair';
-//     const reponse = `Le nombre ${nombre} est ${isPair}`;
-//     console.log(reponse);
-// }
-
-// maFonction (56)
-// nombreEstPair(45)
-
-// // foction recursive
-// const addition = (a, b) => console.log(a + b);
-
-// function test(nbre1, nbre2) {
-//     if (nbre1 > 10) {
-//         addition(nbre1, nbre2);
-//     }
-// }
-// test(11,15);
-
-
-
-// const test2 = () => console.log('Vous êtes majeur.')
-
-// function isAdulte(age, cb) {
-//     if (age >= 18) {
-//         cb();
-//     }
-// }
-
-// isAdulte(17, test2)
-
-// function ageConduire(age, pays) {
-//     age = prompt('Veuillez entrer votre age : ');
-//     pays = prompt('Dans quel pays résidez vous ? (F/EU)');
-//     const capable = (age >= 18 && pays === 'F')||(age >= 16 && pays === 'EU') ? 'pouvez' : 'ne pouvez pas';
-//     if (pays === 'F') {
-//         const capableF = `Vous ${capable} conduire en France`;
-//         console.log(capableF);
-//     } else if (pays === 'EU') {
-//         const capableEU = `Vous ${capable} conduire aux Etats Unis`;
-//         console.log(capableEU);
-//     } else {
-//         console.log('Les données entrées sont éronnées');
-//     }
-// }
-// ageConduire()
-
+console.log(myURL + '/images/monimage.png');
